b9cdfe7c147e48a861c3a59038a6cdb2dbea35f1
HomebrewerChimiboy <tuinstra.tim@gmail.com>
2025-08-02 20:59:16 +0200

    Menus!!!!

---
+++ Assets/scripts/SceneManagerScript.cs
@@ -0,0 +1,61 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using JetBrains.Annotations;
+
+public class SceneManagerScript : MonoBehaviour
+{
+
+    [SerializeField] private Canvas MainMenuCanvas;
+    [SerializeField] private Canvas OptionsCanvas;
+    [SerializeField] private Canvas CreditsCanvas;
+    public void ChangeScene(string levelName)
+    {
+        // Only specifying the sceneName or sceneBuildIndex will load the Scene with the Single mode
+        SceneManager.LoadScene(levelName);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void OpenOptionsWindow()
+    {
+        MainMenuCanvas.gameObject.SetActive(false);
+        OptionsCanvas.gameObject.SetActive(true);
+    }
+
+    public void OpenCreditsWindow()
+    {
+        MainMenuCanvas.gameObject.SetActive(false);
+        CreditsCanvas.gameObject.SetActive(true);
+    }
+
+    public void CloseNonMain()
+    {
+        if (!MainMenuCanvas.gameObject.activeSelf)
+        {
+            MainMenuCanvas.gameObject.SetActive(true);
+        }
+
+        if (OptionsCanvas.gameObject.activeSelf)
+        {
+            OptionsCanvas.gameObject.SetActive(false);
+        }
+
+        if (CreditsCanvas.gameObject.activeSelf)
+        {
+            CreditsCanvas.gameObject.SetActive(false);
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            CloseNonMain();
+        }
+    }
+}

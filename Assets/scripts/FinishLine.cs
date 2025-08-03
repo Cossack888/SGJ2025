26f397ac80d22fb809699bc918ca08bd6669ecf7
Cossack888 <67826646+Cossack888@users.noreply.github.com>
2025-08-03 12:42:37 +0200

    nosacz

---
+++ Assets/scripts/FinishLine.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class FinishLine : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision != null)
+        {
+            if (collision.gameObject.CompareTag("Player"))
+            {
+                MenuManager.Instance.SetActivePanel("win");
+
+            }
+        }
+    }
+}
